c825a92cc99b47df036f5366f6287d046374e47e
Shoyebaktar Shirol <shoyebaktarshirol@gmail.com>
2024-07-16 18:09:52 +0530

    Changes the db name

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','carrental1');
+define('DB_NAME','carrentalfinaldatabase');
 // Establish database connection.
 try
 {
